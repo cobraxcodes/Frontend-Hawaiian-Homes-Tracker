03fd20ae8542d8ed1e7532e48f7776c45150156d
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-07-08 15:28:35 -0700

    feat: add project scaffolding

--- README.md
+++
@@ -1,1 +0,0 @@
-# Frontend-Hawaiian-Homes-Tracker
---
+++ frontend-hawaiianhomestracker/src/App.jsx
@@ -0,0 +1,16 @@
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import { Context } from './Context'
+
+
+function App(){
+   return(
+    <>
+    <BrowserRouter>
+    <nav>
+        
+    </nav>
+
+    </BrowserRouter>
+    </>
+   )
+}
