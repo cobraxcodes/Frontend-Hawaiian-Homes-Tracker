53dbaa85a4037f51f960ac91dbd5f46334df7c12
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-07-10 16:12:49 -0700

    feat: add rank search function with pagination

--- frontend-hawaiianhomestracker/src/search/RankSearch.jsx
+++ frontend-hawaiianhomestracker/src/search/RankSearch.jsx
@@ -6,15 +6,31 @@ import axios from 'axios'
 
 export default function RankSearch(){
     const[page,setPage]=useState(1)
+    const [rank, setRank]=useState("")
+    const [input,setInput]=useState("")
     const URL = `https://hawaiian-homes-tracker.onrender.com/applications?page=${page}&limit=20`
-    const {data,isLoading,isError}=useQuery({
-    queryKey: ['rank', page],
-    queryFn: async () => await axios.get(URL).then(res => res.data),
+
+  const fetchData = async(rank) =>{
+    if(!rank){
+      const res = await axios.get(`https://hawaiian-homes-tracker.onrender.com/applications?page=${page}&limit=20`)
+      return res.data.applications
+    }else{
+      const res = await axios.get(`https://hawaiian-homes-tracker.onrender.com/applications/rank/${rank}`)
+      return res.data.applications
+    }
+  }
+
+      const {data,isLoading,isError,error}=useQuery({
+    queryKey: ['rank',input, page],
+    queryFn: () => fetchData(input, page),
     refetchOnWindowFocus: false,
     keepPreviousData: true,
 })
 
-
+    function handleInput(e){
+      e.preventDefault()
+      setInput(rank.trim())
+    }
     function handleNext(e){
         e.preventDefault()
         setPage(page + 1)
@@ -25,22 +41,53 @@ export default function RankSearch(){
     }
 
     return(
-          <>
+            <>
        <div className="mb-8 text-center">
        <h1 className="text-3xl font-bold text-gray-900  mt-24">Search By Rank</h1>
        </div>
  
 
+    {/* Search Form */}
+     <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+    <form className="max-w-md mx-auto mb-12" onSubmit={handleInput}>
+      <label htmlFor="lastname-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">
+        Search By Rank
+      </label>
+      <div className="relative">
+        <input
+          type="search"
+          id="rank-search"
+          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-100 focus:ring-blue-500 focus:border-blue-500"
+          placeholder="Enter a rank..."
+          value={rank}
+          onChange={(e) => setRank(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+        >
+          Search
+        </button>
+      </div>
+    </form>
+
+
     {/* Loading / Error States */}
-    {isLoading && <p className="text-center">Loading Applications by Rank</p>}
-    {isError && <p className="text-red-600 text-center">"Error fetching applications."</p>}
+    {isLoading && <p className="text-center">Finding Applications With Rankings of {input}</p>}
+    {isError && (
+      <p className="text-red-600 text-center">
+        {error.response?.status === 404
+          ? "No applications found"
+          : "Error fetching applications."}
+      </p>
+    )}
 
 
     {/* Applications Grid */}
-    <div className="grid grid-cols-1 md:grid-cols-1 gap-2 text-center">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
       {!isLoading &&
         !isError &&
-        data?.applications?.map((app) => (
+        data?.map((app) => (
           <div key={app._id} className="bg-white p-6 rounded-lg shadow border">
             <h2 className="text-lg font-bold text-gray-900 mb-2">Name: {app.name}</h2>
             <p className="text-gray-700">
@@ -57,18 +104,15 @@ export default function RankSearch(){
     </div>
 
 
-    <div className="flex items-center gap-24 justify-center border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+        {!input && ( <div className="flex items-center gap-24 justify-center border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
     <button onClick={handlePrev} disabled={page === 1} className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Prev</button>
     <button onClick={handleNext}  className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</button>
-   </div>
+  </div>)}
  
 
-
+      </div>
     </>
-        
-    )
-
-
+)
 }
 
 
